0c2e79f9f3a136197b8ac41abdb62a60a6a61cc6
Michael Nguyen <nguyenmichaelm@yahoo.com>
2023-01-02 10:14:06 -0800

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,6 +51,7 @@ var saveTasks = function () {
 
 // use Moment.js for Due Date Audits
 var auditTask = function (taskEl) {
+  console.log(taskEl);
   // get date from task element
   var date = $(taskEl).find("span").text().trim();
 
@@ -185,7 +186,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -223,15 +224,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function (event) {
     console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
     console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
     console.log("over", event.target);
+    $(this).addClass("dropover-active");
   },
   out: function (event) {
     console.log("out", event.target);
+    $(this).removeClass("dropover-active");
   },
   update: function (event) {
     // array to store the task data in
@@ -274,14 +281,24 @@ $("#trash").droppable({
   drop: function (event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $("bottom-trash").removeClass("bottom-trash-active")
   },
   over: function (event, ui) {
     console.log("over");
+    $("bottom-trash").addClass("bottom-trash-active")
   },
   out: function (event, ui) {
     console.log("out");
+    $("bottom-trash").removeClass("bottom-trash-active")
   }
 });
+
+//use timer to run auditTask() to automate reload every 30 mins
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
